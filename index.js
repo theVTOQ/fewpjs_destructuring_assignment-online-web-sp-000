de0e1f13b76e791001c05f47ecdc4b62b639bffc
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 18:38:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 const [red, orange, yellow, green, blue, indigo, violet] = colors;
 const [r, o, y, g, b, , v] = colors;
 const [, , , , , indg, ] = colors;
+
 const muppet = {
   name: 'Kermit',
   color: 'green',
