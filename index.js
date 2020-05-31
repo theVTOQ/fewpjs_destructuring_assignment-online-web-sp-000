69478d78876e5e131626ecd4d834d734e0434ee1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 18:41:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
-
+const {song_2, song_4} = k_muppet.k_album.the_muppet_movie;
+const {k_job, k_partner} = k_muppet;
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
