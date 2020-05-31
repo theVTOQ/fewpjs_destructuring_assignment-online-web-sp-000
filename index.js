591abd2dfb25bb9d10478fc0419e726b8c3a9d6b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 18:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const k_muppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
