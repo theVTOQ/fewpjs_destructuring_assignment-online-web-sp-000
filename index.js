6a1a92d260864190aa8de78fa8d5f85ed014d0d1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 18:28:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const farmAnimals = 'cow horse sheep pig chicken'
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
